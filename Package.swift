4ef77f73934e6bbaf6f14271dd89c734588fd0a2
macOS CI <sdk.accounts@banuba.com>
2023-11-24 18:37:39 +0300

    1.9.1-86-gb002ed1e38

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.9.1-86-g65c1c833d4"
+let bnbPackageVersion: Version = "1.9.1-86-gb002ed1e38"
 
 let package = Package(
     name: "BNBHands",
@@ -36,8 +36,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBHands",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-86-g65c1c833d4/BNBHands.zip",
-            checksum: "cd2ba2e939bf8e53a12fb9e43b83d3ab733bc75782e1f9c71d193ae0f6b0b34f"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-86-gb002ed1e38/BNBHands.zip",
+            checksum: "69bff2d8344062e516f95edb57d889c6dae7f92ddc7691411ae9d1fc1fa053fe"
         ),
         .target(
             name: "BNBHands_BNBSdkCore",
